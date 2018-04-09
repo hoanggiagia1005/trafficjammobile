6c79c1bfa2c44d54a5e58b948be89c4fe56c4623
Hoang <tranthehoangk60@gmail.com>
2018-04-09 22:56:33 +0700

    no message

--- app/src/main/java/trafficjam/k60n/com/trafficjam/util/Utils.java
+++ app/src/main/java/trafficjam/k60n/com/trafficjam/util/Utils.java
@@ -3,4 +3,6 @@ package trafficjam.k60n.com.trafficjam.util;
 public class Utils {
     public static final int PLACE_AUTOCOMPLETE_START= 1;
     public static final int PLACE_AUTOCOMPLETE_END= 2;
+
+    
 }
